fd2afaabe2ef7b2dd9d72c41e46fcf2041439450
cyschneck <22159116+cyschneck@users.noreply.github.com>
2024-05-12 19:58:35 -0600

    send alerts based on user/topics

--- notifications_for_user.py
+++ notifications_for_user.py
@@ -1,4 +1,5 @@
 from pyinaturalist import *
+
 import string
 import sys
 import requests
@@ -6,62 +7,49 @@ from time import sleep
 from datetime import datetime, timedelta, time
 
 user = sys.argv[1]
-last_check_datetime = datetime.now() - timedelta(days=0, hours=12, minutes=6)
+nfty_id = sys.argv[2]
+last_check_datetime = datetime.now() - timedelta(days=0, hours=10, minutes=6)
 
 def getObservations(user_id):
-	print("\n")
-	recent_observations = get_observations(user_id=user_id)#, d1=last_check_datetime)
+	recent_observations = get_observations(user_id=user_id, d1=last_check_datetime)
 	observations = recent_observations["results"]
 	for observation in observations:
 		#pprint(observation)
+
+		# Identification with common name based on best guess
 		id_guess = observation["species_guess"]
 		if id_guess is None:
-			id_guess = observation["taxon"]["preferred_common_name"]
+			if "preferred_common_name" in observation["taxon"].keys():
+				id_guess = observation["taxon"]["preferred_common_name"]
+			elif "name" in observation["taxon"].keys():
+				id_guess = observation["taxon"]["name"]
+			else:
+				id_guess = "Something Strange"
 		id_guess = string.capwords(id_guess)
-		print(f"{user} spotted {id_guess}")
-		previous_observations = get_observations(user_id=user_id, taxon_name=id_guess)["results"]
-		#pprint(previous_observations)
-		if len(previous_observations) > 1 and len(previous_observations) <= 14:
-			print(f"Seen before {len(previous_observations)} times")
-		if len(previous_observations) > 15:
-			print(f"Seen before a lot! ({len(previous_observations)} times)")
-		if len(previous_observations) <= 1:
-			print("Never seen before")
-		location_guess = observation["place_guess"]
-		print(location_guess)
-		print(observation["uri"])
-		print(observation["taxon"]["default_photo"]["square_url"])
-		print(observation["taxon"]["iconic_taxon_name"])
-		print("\n")
-		#break
-		"""
-		print(observation.keys())
-		print("\n")
-		inaturalist_id = observation["id"]
-		print(f"id = {inaturalist_id}")
-		inaturalist_created_at = observation["created_at"]
-		print(inaturalist_created_at)
-		inaturalist_id_count = observation["identifications_count"]
-		print(f"id count = {inaturalist_id_count}")
-		inaturalist_species_guess = observation["species_guess"]
-		if inaturalist_species_guess is None: inaturalist_species_guess = "Something Mysterious"
-		print(f"species_guess = {inaturalist_species_guess}")
-		inaturalist_location = observation["place_guess"]
-		print(inaturalist_location)
-		# something seen before (Again!) versus something new ("For the first time")
-		inaturalist_observation_photos = observation["observation_photos"]
-		inaturalist_square_img_url = []
-		if len(inaturalist_observation_photos) > 0:
-			for observation_photo in inaturalist_observation_photos:
-				inaturalist_square_img_url.append(observation_photo["photo"]["url"])
-		print(inaturalist_square_img_url)
-		inaturalist_observation_url = f"https://www.inaturalist.org/observations/{inaturalist_id}"
-		print(inaturalist_observation_url)
-		break
-		"""
-	return observations
 
-def testRequest():
+		# Track if observation is new or previously seen
+		if id_guess != "Something Strange":
+			previous_observations = get_observations(user_id=user_id, taxon_name=id_guess)["results"]
+			#pprint(previous_observations)
+			if len(previous_observations) > 1 and len(previous_observations) <= 14:
+				seen_previous = " (again!) "
+			if len(previous_observations) > 15:
+				seen_previous = " (a lot!) "
+			if len(previous_observations) <= 1:
+				seen_previous = " (for the first time!) "
+
+		# Retrieve information about location, link to observation, and default img
+		location_guess = observation["place_guess"]
+		observation_link = observation["uri"]
+		img_url = observation["taxon"]["default_photo"]["square_url"]
+		taxon_id = observation["taxon"]["iconic_taxon_name"]
+
+		# Send observation as request
+		observation_data = f"{user} spotted {id_guess}{seen_previous}in {location_guess}"
+		print(f"Sending new observation: {id_guess}")
+		sendRequest(observation_data, observation["taxon"]["iconic_taxon_name"], observation["uri"], observation["taxon"]["default_photo"]["square_url"])
+
+def sendRequest(data_string, taxon, url, icon_img):
 	plant = "herb"         # Plantae
 	fungi = "mushroom"     # Fungi
 	insect = "bug"         # Insecta
@@ -70,20 +58,39 @@ def testRequest():
 	fish = "tropical_fish" # Actinopterygii
 	arachnid = "spider"    # Arachnida
 	reptile = "snake"      # Reptilia
-	amphibia = "frog"      # Amphibia
+	amphibian = "frog"      # Amphibia
 	mollusk = "snail"      # Mollusca
 	other = "worm"         # Animalia
 	unknown = "feet"       # Unknown
 
-	requests.post("https://ntfy.sh/testing314",
-				data=f"{user} spotted a Brown Rat (Again!) in Tarturus",
+	taxon_dict = {"Plantae": plant,
+				  "Fungi": fungi,
+				  "Insecta": insect,
+				  "Aves": bird,
+				  "Mammalia": mammal,
+				  "Actinopterygii": fish,
+				  "Arachnida": arachnid,
+				  "Reptilia": reptile,
+				  "Amphibia": amphibian,
+				  "Mollusca": mollusk,
+				  "Animalia": other
+				  }
+
+	# Set tags based on taxon, with default tag for unknown type
+	if taxon in taxon_dict:
+		taxon_tag = taxon_dict[taxon]
+	else:
+		taxon_tag = unknown
+
+	# testing request: testing314
+	requests.post(f"https://ntfy.sh/{nfty_id}",
+				data=data_string,
 				headers = {
 					"Title": "New Observation!",
-					"Tags": mammal,
-					"Click": "https://www.inaturalist.org/observations/215126231",
-					"Icon":"https://avatars.githubusercontent.com/u/22159116?v=4"
+					"Tags": taxon_tag,
+					"Click": url,
+					"Icon":icon_img
 				})
 
 if __name__ == '__main__':
-	#getObservations(user)
-	testRequest()
+	getObservations(user)
